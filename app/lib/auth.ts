f7058a93dca9f6ab7ddeeaad08a64b015b686600
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2026-02-04 15:12:19 +0100

    Deployment problems

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -5,6 +5,8 @@ import * as schema from "@/app/lib/db/schema";
 import {nextCookies} from "better-auth/next-js"; 
 
 export const auth = betterAuth({ 
+    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    secret: process.env.BETTER_AUTH_SECRET!,
     emailAndPassword: { 
         enabled: true, // On active les comptes par email et mot de passe 
     }, 
